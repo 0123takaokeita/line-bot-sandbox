06ccd52f1b86d7097ce09fc7787cb93bf20f1aeb
KeitaTakao <k.takao@neogenia.co.jp>
2023-10-18 01:36:53 +0900

    feat: component renderring syntax sugger

--- rails/app/helpers/application_helper.rb
+++ rails/app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # componentのrenderのsyntax sugger
+  # @param [String] name componentの名前
+  # @param [Array] args componentのinitializeに渡す引数
+  # @param [Hash] kwargs componentのinitializeに渡すキーワード引数
+  # @param [Proc] block componentのbody
+  def component(name, *args, **kwargs, &block)
+    component = name.to_s.camelize.constantize::Component
+    render(component.new(*args, **kwargs), &block)
+  end
 end
